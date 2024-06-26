78769f028f8dc43c80652e51a437b1f099d5cee2
Alexander Bocken <alexander@bocken.org>
2024-06-26 06:26:36 +0200

    tagmon + focusmon

--- dwm.c
+++ dwm.c
@@ -219,6 +219,7 @@ static void spawn(const Arg *arg);
 static void spiral(Monitor *mon);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
+static void tagfocusmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
@@ -1791,6 +1792,11 @@ tagmon(const Arg *arg)
 	}
 }
 
+void
+tagfocusmon(const Arg *arg){
+	tagmon(arg);
+	focusmon(arg);
+}
 void
 tile(Monitor *m)
 {
